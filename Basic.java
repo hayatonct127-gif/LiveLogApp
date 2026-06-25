b44038ad521e25fba9ee5bb885689ddfe5d37b9c
hayato <hayato.nct127@gmail.com>
2026-06-25 22:48:13 +0900

    if文復習

--- Basic.java
+++ Basic.java
@@ -17,5 +17,55 @@ public class Basic {
         System.out.println(name);
         System.out.println(member);
 
+        if(band.equals("ONE OK ROCK")) {
+            System.out.println("日本のロックバンドです");
+        } else if(band.equals("Linkin Park")) {
+            System.out.println("アメリカのロックバンドです");
+        } else {
+            System.out.println("イギリスのロックバンドです");
+        }
+
+        String country = "日本";
+        int memberCount = 4;
+
+        if(country.equals("日本") && memberCount >= 4) {
+            System.out.println("日本の4ピースロックバンドです");
+        }
+
+        if(bandName.equals("BRING ME THE HORIZON")|| bandName.equals("The Beatles")) {
+            System.out.println("イギリスのロックバンドです");
+        }
+
+        boolean actives = true;
+
+        if(!actives) {
+            System.out.println("活動休止しています");
+        }
+
+        String keyword = "";
+        if(keyword.isEmpty()) {
+            System.out.println("入力してください");
+        }
+
+        if(name == null || name.isEmpty()) {
+            System.out.println("必須項目です");
+        } 
+        
+        if(country.equals("日本")) {
+            System.out.println("国内バンド");
+        } else {
+            System.out.println("海外バンド");
+        }
+        memberCount = 5;
+        active = true;
+
+        if(memberCount >= 4 && active) {
+            System.out.println("活動中の大型バンド");
+        }
+        band = "ONE OK ROCK";
+
+        if(band.equals("ONE OK ROCK") || band.equals("Nirvana")) {
+            System.out.println("有名ロックバンド");
+        }
     }
 }
